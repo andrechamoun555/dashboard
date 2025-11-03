a7a7e2c5acf77f870f2c9db41662092b6cfa518d
André Chamoun <ac223hy@student.lnu.se>
2025-11-03 23:03:11 +0100

    Worksgit add .

---
+++ src/main/java/com/chamoundigital/ai/AiconsultApplication.java
@@ -0,0 +1,13 @@
+package com.chamoundigital.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiconsultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiconsultApplication.class, args);
+	}
+
+}
